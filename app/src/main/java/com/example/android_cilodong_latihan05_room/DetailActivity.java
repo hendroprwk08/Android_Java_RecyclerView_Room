73454b453d9792e629259f605993184a3f27b728
Hendro Purwoko <hendroprwk08@gmail.com>
2019-05-25 09:40:53 +0700

    ganti layout detail

--- app/src/main/java/com/example/android_cilodong_latihan05_room/DetailActivity.java
+++ app/src/main/java/com/example/android_cilodong_latihan05_room/DetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
@@ -18,7 +19,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         final EditText et_nama = (EditText) findViewById(R.id.det_et_nama);
-        final EditText et_kelas = (EditText) findViewById(R.id.det_et_kelas);
+        //final EditText et_kelas = (EditText) findViewById(R.id.det_et_kelas);
+        final Spinner sp_kelas = (Spinner) findViewById(R.id.sp_det_kelas);
         final Button bt_ubah = (Button) findViewById(R.id.det_bt_ubah);
         final Button bt_hapus = (Button) findViewById(R.id.det_bt_hapus);
 
@@ -29,7 +31,10 @@ public class DetailActivity extends AppCompatActivity {
         //letakkan isi bundle
         final int id = bundle.getInt("b_id", 0);
         et_nama.setText(bundle.getString("b_nama"));
-        et_kelas.setText(bundle.getString("b_kelas"));
+
+        //set spinner
+        int idx_spinner = setSpinner(sp_kelas, bundle.getString("b_kelas"));
+        sp_kelas.setSelection(idx_spinner);
 
         getSupportActionBar().setTitle("Data Peserta"); // for set actionbar title
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // for add back arrow in action back
@@ -38,10 +43,10 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 nama =  et_nama.getText().toString();
-                kelas =  et_nama.getText().toString();
+                kelas =  sp_kelas.getSelectedItem().toString();//et_kelas.getText().toString();
 
                 MainActivity.db.siswaDAO().update(new Siswa(id, nama, kelas));
-                Toast.makeText(getApplicationContext(), "Data " + et_nama.getText().toString() + " berhasil diubah", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Data " + nama + " berhasil diubah", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
@@ -50,12 +55,22 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 nama =  et_nama.getText().toString();
-                kelas =  et_nama.getText().toString();
+                kelas =  sp_kelas.getSelectedItem().toString();//et_kelas.getText().toString();
 
                 MainActivity.db.siswaDAO().delete(new Siswa(id, nama, kelas));
-                Toast.makeText(getApplicationContext(), "Data " + et_nama.getText().toString() + " dihapus", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Data " + nama + " dihapus", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
+    }
+
+    private int setSpinner(Spinner spinner, String myString){
+        for (int i=0;i<spinner.getCount();i++){
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)){
+                return i;
+            }
+        }
+
+        return 0;
     }
 }
